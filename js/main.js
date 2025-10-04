29dd626842d9ce53cba210a85db59abc0b64c022
Akshay Wagh <waghakshay186@gmailcom>
2025-10-04 20:08:41 +0530

    contact form connected

--- js/main.js
+++ js/main.js
@@ -83,3 +83,15 @@
 
 })(jQuery);
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
+      bsCollapse.hide(); // navbar close karega
+    });
+  });
+});
